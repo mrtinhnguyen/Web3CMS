8c93910cfaec6785bc3ea7201ab5382a2333b91b
mbeliogl <maximbelioglo@gmail.com>
2025-10-29 19:35:19 -0400

    Change home btn in header component to explore

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { House, LayoutDashboard, PenTool } from 'lucide-react';
+import { House, LayoutDashboard, PenTool, BookOpen } from 'lucide-react';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 function Header() {
@@ -10,7 +10,7 @@ function Header() {
           <h1>Penny.io</h1>
         </Link>
         <nav className="nav">
-          <Link to="/" className="nav-link"><House size={16}/>Home</Link>
+          <Link to="/explore" className="nav-link"><BookOpen size={16}/>Explore</Link>
           <Link to="/write" className="nav-link"><PenTool size={16}/>Write</Link>
           <Link to="/dashboard" className="nav-link"><LayoutDashboard size={16}/>Dashboard</Link>
           <ConnectButton />
